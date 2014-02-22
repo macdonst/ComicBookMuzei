a8d55d828de61817165ae9371ca51547bfa72529
Simon MacDonald <simon.macdonald@gmail.com>
2014-02-22 12:18:14 -0500

    Add icons and token to art source call

--- src/com/simonmacdonald/muzei/comic/ComicCoverArtSource.java
+++ src/com/simonmacdonald/muzei/comic/ComicCoverArtSource.java
@@ -16,16 +16,17 @@
 
 package com.simonmacdonald.muzei.comic;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import android.content.Intent;
 import android.net.Uri;
-import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.android.apps.muzei.api.Artwork;
 import com.google.android.apps.muzei.api.RemoteMuzeiArtSource;
 import com.simonmacdonald.muzei.comic.ComicCoverService.Cover;
-
-import java.util.Random;
 
 import retrofit.ErrorHandler;
 import retrofit.RequestInterceptor;
@@ -34,7 +35,8 @@ import retrofit.RestAdapter.LogLevel;
 import retrofit.RetrofitError;
 
 public class ComicCoverArtSource extends RemoteMuzeiArtSource {
-    private static final String TAG = "ComicCoverArtSource";
+    private static final String BY_LINE = "Data provided by Marvel. © 2014 Marvel";
+	private static final String TAG = "ComicCoverArtSource";
     private static final String SOURCE_NAME = "ComicCoverArtSource";
 
     private static final int ROTATE_TIME_MILLIS = 3 * 60 * 60 * 1000; // rotate every 3 hours
@@ -82,25 +84,13 @@ public class ComicCoverArtSource extends RemoteMuzeiArtSource {
         if (response == null) {
             throw new RetryException();
         }
-
-//        if (response.photos.size() == 0) {
-//            Log.w(TAG, "No photos returned from API.");
-//            scheduleUpdate(System.currentTimeMillis() + ROTATE_TIME_MILLIS);
-//            return;
-//        }
-//
-//        Random random = new Random();
-//        Photo photo;
-//        String token;
-//        while (true) {
-//            photo = response.photos.get(random.nextInt(response.photos.size()));
-//            token = Integer.toString(photo.id);
-//            if (response.photos.size() <= 1 || !TextUtils.equals(token, currentToken)) {
-//                break;
-//            }
-//        }
         
-        // <img id="image-1" class="cover" src="http://i.annihil.us/u/prod/marvel/i/mg/6/80/5284ea82357da.jpg">
+        String token;
+        if (currentToken == null) {
+        	token = "1";
+        } else {
+        	token = "" + (Integer.valueOf(currentToken) + 1) ;
+        }
 
         Log.d(TAG, "title = " + response.title);
         Log.d(TAG, "author = " + response.date);
@@ -108,9 +98,9 @@ public class ComicCoverArtSource extends RemoteMuzeiArtSource {
         
         publishArtwork(new Artwork.Builder()
                 .title(response.title)
-                .byline("Data provided by Marvel. © 2014 Marvel")
+                .byline(BY_LINE)
                 .imageUri(Uri.parse(response.url))
-                //.token(token)
+                .token(token)
                 .viewIntent(new Intent(Intent.ACTION_VIEW,
                         Uri.parse(response.url)))
                 .build());
