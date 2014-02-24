f5c05dc264872d110cc081db935d293a328eaa94
Simon MacDonald <simon.macdonald@gmail.com>
2014-02-24 00:02:48 -0500

    Begin to work on settings panel

--- src/com/simonmacdonald/muzei/comic/ComicCoverSettingsActivity.java
+++ src/com/simonmacdonald/muzei/comic/ComicCoverSettingsActivity.java
@@ -1,8 +1,15 @@
 package com.simonmacdonald.muzei.comic;
 
-import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
 
-public class ComicCoverSettingsActivity extends FragmentActivity  {
+public class ComicCoverSettingsActivity extends PreferenceActivity  {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.addPreferencesFromResource(R.xml.settings);
+    }
 
 }
